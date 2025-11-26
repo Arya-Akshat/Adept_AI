f2e5169652f812cffc0bdffad7b9cc2dc65dc517
Arya-Akshat <akshat.arya13@gmail.com>
2025-11-27 00:20:03 +0530

    Fix: Prevent app crash on DB connection failure

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 import { MONGO_URI } from "../constants/env";
 
 
-const connectToDatabase = async() => {
+const connectToDatabase = async () => {
     try {
         await mongoose.connect(MONGO_URI);
         console.log("Succesfully connected to DB");
     } catch (error) {
         console.log('Could not connect to database', error);
-        process.exit(1);
+        console.log('Could not connect to database', error);
+        // process.exit(1); // Don't crash if DB is missing, we use JSON for now
     }
 }
 export default connectToDatabase;
